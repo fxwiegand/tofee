8853ae53eb2bb1bda92d4c0d1ee2f7b7f67d81e0
FSieber <fabian.sieber@tu-dortmund.de>
2020-03-29 23:27:56 +0200

    established Dortmund-Design

--- tofee/settings.py
+++ tofee/settings.py
@@ -88,9 +88,9 @@ BOOTSTRAP4 = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'tofee/static')
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'tofee/media')
 MEDIA_URL = '/media/'
 
 LOGIN_REDIRECT_URL = '/polls/'
