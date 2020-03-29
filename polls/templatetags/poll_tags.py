2ddb9bc0b8b781dfd23bbf484969220f8a3baa40
Felix Wiegand <fxwiegand@gmail.com>
2020-03-30 00:08:33 +0200

    display results in detail view if voting has ended

--- polls/templatetags/poll_tags.py
+++ polls/templatetags/poll_tags.py
@@ -9,5 +9,4 @@ def percentage(part, total):
     """
     get part percentage of total
     """
-    print('part:', part, type(part), 'total:', total, type(total))
     return round(part * 100 / total) if total > 0 else 0
